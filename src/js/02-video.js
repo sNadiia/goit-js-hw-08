f1a2c604f8450202767d40f1d884600097dfca04
Nadiia Samborska <dilova_3@ukr.net>
2023-04-28 23:52:29 +0300

    task-2 without throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,56 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const onPlay = function (e) {
+  const currentTime = e.seconds;
+
+  console.log('video played!');
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+  save(STORAGE_KEY, currentTime);
+};
+
+player.on('timeupdate', onPlay);
+
+const timeForLoad = load(STORAGE_KEY);
+player
+  .setCurrentTime(timeForLoad)
+  .then(function (seconds) {
+    // console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
