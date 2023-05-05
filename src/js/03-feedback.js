7d8ab3bed38042f053b07bad142b7dee83f476ea
Nadiia Samborska <dilova_3@ukr.net>
2023-05-05 19:14:47 +0300

    change task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,10 @@
-// обрати елементів DOM для роботи;
-// зберегти поточні поля форми в локал;
-// після перезавантаження внести дані з локалу(якщо там є ) в поля форми;
-// стерти поля форми та вивести в консоль значення полів під час сaбміту;
-// тротле добавити.
 import throttle from 'lodash.throttle';
 
 const feedbackFormRef = document.querySelector('.feedback-form');
-const input = document.querySelector('input');
-const textArea = document.querySelector('textarea');
-const button = document.querySelector('button');
-const STORAGE_KEY = 'feedback-form-state';
-const currentValueOfForm = {};
+const { email, message } = feedbackFormRef.elements;
 
-let IsFillEmail = false;
-let IsFillMessage = false;
-// console.log(IsFillEmail);
-// console.log(IsFillMessage);
-// console.dir(input);
-// console.log(input.value);
-// console.dir(textArea);
-// console.log(textArea.value);
+const currentValueOfForm = {};
+const STORAGE_KEY = 'feedback-form-state';
 
 const save = (key, value) => {
   try {
@@ -29,7 +14,6 @@ const save = (key, value) => {
     console.error('Set state error: ', error.message);
   }
 };
-
 const load = key => {
   try {
     const serializedState = localStorage.getItem(key);
@@ -39,77 +23,44 @@ const load = key => {
   }
 };
 
-feedbackFormRef.addEventListener('input', throttle(getCurrentValueOfForm, 500));
+feedbackFormRef.addEventListener(
+  'input',
+  throttle(saveDataInLocaleStorage, 500, (leading = false))
+);
+window.addEventListener('DOMContentLoaded', fillFromStorage);
+feedbackFormRef.addEventListener('submit', handleSubmit);
 
-function getCurrentValueOfForm(e) {
-  const nameField = e.target.tagName;
-  const valueOfField = e.target.value.trim();
-
-  if (valueOfField === '') {
-    alert('Потрібно ввести текст!');
-    return;
-  }
-  {
-    if (nameField === 'INPUT') {
-      currentValueOfForm.email = valueOfField;
-      IsFillEmail = true;
-      // console.log(IfFillEmail);
-      save(STORAGE_KEY, currentValueOfForm);
-    } else {
-      if (nameField === 'TEXTAREA') {
-        currentValueOfForm.message = valueOfField;
-        IsFillMessage = true;
-        // console.log(IfFillMessage);
-        save(STORAGE_KEY, currentValueOfForm);
-      }
-    }
-  }
+function saveDataInLocaleStorage(e) {
+  currentValueOfForm.email = email.value.trim();
+  currentValueOfForm.message = message.value.trim();
+  save(STORAGE_KEY, currentValueOfForm);
 }
 
-window.addEventListener('DOMContentLoaded', fillFromStorage);
 function fillFromStorage(e) {
   const currentState = load(STORAGE_KEY);
-
-  if (currentState.email) {
-    input.value = currentState.email;
-  }
-  // console.log(input.value);
-
-  if (currentState.message) {
-    textArea.value = currentState.message;
-  }
-  // console.log(textArea.value);
-
-  if (input.value && textArea.value) {
-    IsFillMessage = true;
-    IsFillEmail = true;
+  if (currentState) {
+    email.value = currentState.email;
+    message.value = currentState.message;
   }
 }
 
-button.addEventListener('click', handleSubmit);
-function handleSubmit(e) {
-  e.preventDefault();
-  if (IsFillMessage && IsFillEmail) {
-    console.log(load(STORAGE_KEY));
-    clearStorage();
-    clearForm();
-    IsFillMessage = false;
-    IsFillEmail = false;
-  } else {
+function handleSubmit(ev) {
+  ev.preventDefault();
+  const valueEmail = ev.srcElement[0].value;
+  const valueMessage = ev.srcElement[1].value;
+  const getDataOnSubmit = {};
+
+  if (valueEmail === '' || valueMessage === '') {
     alert('Незаповнені поля! Введіть дані!');
     return;
+  } else {
+    getDataOnSubmit.email = valueEmail;
+    getDataOnSubmit.message = valueMessage;
+    console.log(getDataOnSubmit);
+    localStorage.removeItem(STORAGE_KEY);
+    feedbackFormRef.reset();
   }
 }
 
-function clearStorage() {
-  save(STORAGE_KEY, {});
-}
 
-function clearForm() {
-  input.value = '';
-  textArea.value = '';
-}
-// console.dir(input);
-// console.log(input.value);
-// console.dir(textArea);
-// console.log(textArea.value);
+
